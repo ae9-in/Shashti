42f64451f76c0317dbf7026e83444127dea513bd
jishnu87654321 <jishnu87654321@gmail.com>
2026-06-23 21:25:01 +0530

    fix: relocate backend router and db client to api folder to fix ERR_MODULE_NOT_FOUND on Vercel

--- server/index.ts
+++ server/index.ts
@@ -3,7 +3,7 @@ import { createServer } from "http";
 import path from "path";
 import fs from "fs";
 import { fileURLToPath } from "url";
-import { registerRoutes } from "./routes";
+import { registerRoutes } from "../api/routes";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
